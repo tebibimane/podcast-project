b2b04a3e1319e259b1cc5eb2d2a6825d9b3a91dc
Amin <oo2014oo@gmail.com>
2025-04-16 15:28:16 +0100

    refactor

--- pages/profiles/moussa.js
+++ pages/profiles/moussa.js
@@ -1,5 +1,5 @@
 let playButton = document.querySelector('.play-movie');
-let video = document.querySelector('.viedo-container');
+let video = document.querySelector('.video-container');
 let closebtn = document.querySelector('.close-video');
 playButton.onclick =() =>{
     video.classList.add("show-video");
